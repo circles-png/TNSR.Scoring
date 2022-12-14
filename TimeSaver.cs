6bf3ff2a599985435213d9b32a33eb42bbfd037d
circles-png <matthew.li10@education.nsw.gov.au>
2022-12-14 17:22:35 +1100

    doc comments

--- TimeSaver.cs
+++ TimeSaver.cs
@@ -1,11 +1,24 @@
 ﻿using System.Text.Json;
 
 namespace TNSR.Scoring;
+
+/// <summary>
+///     Utility class for saving and loading level finishing times for the game TNSR.
+/// </summary>
 public static class TimeSaver
 {
+    /// <summary>
+    ///     The path to the file where the times are saved.
+    /// </summary>
     public static string SavePath { get; set; } = "";
     static LevelTimeData _timeData = new();
 
+    /// <summary>
+    ///     Save the time taken to complete a level.
+    /// </summary>
+    /// <param name="levelNumber">Number of the level to save the score to.</param>
+    /// <param name="timeTaken">Time taken to complete the level.</param>
+    /// <param name="customTime">Optional custom time to use instead of the current time.</param>
     public static void SaveTime(int levelNumber, float timeTaken, DateTime? customTime = null)
     {
         if (_timeData.All(level => level.LevelNumber != levelNumber))
@@ -14,6 +27,12 @@ public static class TimeSaver
             .Times
             .Add(new Time(timeTaken, customTime ?? DateTime.Now));
     }
+    /// <summary>
+    ///     Get the best time for a level.
+    /// </summary>
+    /// <param name="levelNumber">Number of the level to get the best time for.</param>
+    /// <returns>The best time for the level specified.</returns>
+    /// <exception cref="Exception">There are no times saved on the level.</exception>
     public static Time GetBestTime(int levelNumber)
     {
         return _timeData[levelNumber]
@@ -21,6 +40,11 @@ public static class TimeSaver
             .OrderBy(time => time.TimeTaken)
             .FirstOrDefault() ?? throw new Exception($"No times for level {levelNumber}");
     }
+    /// <summary>
+    ///     Remove all times for a level except the best time and the most recent times.
+    /// </summary>
+    /// <param name="levelNumber">Number of the level for cleaning times.</param>
+    /// <param name="recent">Number of recent scores to save. Defaults to 5.</param>
     public static void CleanTimes(int levelNumber, int recent = 5)
     {
         var times = new List<Time>() { GetBestTime(levelNumber) };
@@ -32,6 +56,10 @@ public static class TimeSaver
         );
         _timeData[levelNumber].Times = times;
     }
+    /// <summary>
+    ///     Write the time data to the file specified in <see cref="SavePath"/>.
+    /// </summary>
+    /// <exception cref="Exception">The path for saving times is not set.</exception>
     public static void WriteToFile()
     {
         if (SavePath == "")
@@ -48,19 +76,25 @@ public static class TimeSaver
             )
         );
     }
+    /// <summary>
+    ///    Load the time data from the file specified in <see cref="SavePath"/>.
+    /// </summary>
+    /// <exception cref="Exception">The path for saving times is not set.</exception>
     public static void ReadFromFile()
     {
         if (SavePath == "")
             throw new Exception("SavePath not set");
-        var file = new FileStream(SavePath, FileMode.Open);
+        var file = File.ReadAllText(SavePath);
         if (file.Length == 0)
             _timeData = new();
         else
             _timeData = JsonSerializer
                 .Deserialize<LevelTimeData>(file)
                 ?? throw new Exception("Failed to read file");
-        file.Dispose();
     }
+    /// <summary>
+    ///     Clear all time data in memory.
+    /// </summary>
     public static void ClearData()
     {
         _timeData = new();
